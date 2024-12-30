559f8fdf902f109b7856a3b5433e546999bc17e3
theonlytruealex <alexiova264@gmail.com>
2024-12-30 22:25:32 +0200

    Fixed: next_fit now O(1) space complexity

--- Algorithms/next_fit.cpp
+++ Algorithms/next_fit.cpp
@@ -2,18 +2,19 @@
 #include <fstream>
 #include <vector>
 
-int nf(int items[], int itemCount, int binCap, std::vector<int>& bins, int placements[]) {
-    bins.push_back(items[0]);
+int nf(int items[], int itemCount, int binCap, int placements[]) {
+    int currBin = items[0], binCount = 1;
     placements[0] = 0;
     for (int i = 1; i < itemCount; i++) {
-        if (bins.back() + items[i] > binCap) {
-            bins.push_back(items[i]);
+        if (currBin+ items[i] > binCap) {
+            currBin = items[i];
+            binCount++;
         } else {
-            bins.back() += items[i];
+            currBin += items[i];
         }
-        placements[i] = bins.size() - 1;
+        placements[i] = binCount - 1;
     }
-    return bins.size();
+    return binCount;
 }
 
 int main() {
@@ -21,17 +22,15 @@ int main() {
     std::ofstream fout("outputs/nf.out");
 
     int itemCount, binCap;
-    std::vector<int> bins;
     
     fin >> itemCount >> binCap;
     int items[itemCount], placements[itemCount];
-    bins.reserve(itemCount);
 
     for (int i = 0; i < itemCount; i++) {
         fin >> items[i];
     }
 
-    int minCount = nf(items, itemCount, binCap, bins, placements);
+    int minCount = nf(items, itemCount, binCap, placements);
     fout << minCount << "\n";
     for (int i = 0; i < minCount; i++) {
         fout << "Bin number " << i << ": ";
