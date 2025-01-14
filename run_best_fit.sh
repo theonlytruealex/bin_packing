c5116892d50fa40db3475b56e9614919cc006ecc
theonlytruealex <alexiova264@gmail.com>
2025-01-14 20:40:31 +0200

    Added: more test files

---
+++ run_best_fit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+
+# Definirea căilor
+INPUT_DIR="second_input"
+OUTPUT_DIR="out"
+EXECUTABLE="src/best_fit"
+
+# Creare directoare de output pentru fiecare executabil
+mkdir -p "$OUTPUT_DIR"
+
+exe_name=$(basename "$EXECUTABLE")
+mkdir -p "$OUTPUT_DIR/$exe_name"
+
+# Procesarea fiecărui fișier de input
+for input_file in "$INPUT_DIR"/*; do
+    input_base=$(basename "$input_file")
+    
+
+    exe_name=$(basename "$EXECUTABLE")
+    output_file="$OUTPUT_DIR/$exe_name/$input_base.out"
+ 
+    # Rulează executabilul cu fișierul de intrare
+    if [[ -x "$EXECUTABLE" ]]; then
+        ./"$EXECUTABLE" < "$input_file" > /dev/null
+    else
+        echo "Eroare: $EXECUTABLE nu este executabil. Verifică compilarea!"
+    fi
+done
+
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+echo "Timpul total de execuție: $ELAPSED_TIME secunde"
---
+++ second_generate.py
@@ -0,0 +1,29 @@
+import os
+import random
+import subprocess
+
+# Define folder paths and bin capacity
+INPUT_FOLDER = "second_input/"
+
+BIN_CAPACITY = 500
+MTP_EXECUTABLE = "src/mtp"
+
+# Ensure folders exist
+os.makedirs(INPUT_FOLDER, exist_ok=True)
+
+def generate_tests_and_refs(num_tests=1000, num_items_range=(10000, 10000), weight_range=(1, BIN_CAPACITY)):
+    for i in range(num_tests):
+        print(i)
+        # Generate input: number of items and their weights
+        num_items = random.randint(*num_items_range)
+        weights = [random.randint(*weight_range) for _ in range(num_items)]
+
+        # Write input to file
+        input_file_path = f"{INPUT_FOLDER}/test_{i}.txt"  # Define input file path
+        with open(input_file_path, "w") as input_file:
+            input_file.write(f"{len(weights)} {BIN_CAPACITY}\n")
+            input_file.write(" ".join(map(str, weights)))
+
+
+# Generate 2000 test cases
+generate_tests_and_refs()
